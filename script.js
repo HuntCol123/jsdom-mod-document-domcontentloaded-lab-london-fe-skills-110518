0681e0e485d8a41b41da950f5ae93386b354c1bb
Hunt Collings <hunt@collings.co.uk>
2018-12-17 20:23:45 +0000

    Automatically backed up by Learn

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ document.addEventListener("DOMContentLoaded", e => {
 document.querySelector("body").addEventListener("click", e => console.log("Reggae, Reggae!"));})
 
 
-document.addEventListener("DOMContentLoaded", e => {
+
 document.getElementById("hidden-div").innerHTML = "301"; })
